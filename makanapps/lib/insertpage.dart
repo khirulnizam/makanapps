ebb1dbfab00ab0c9eeef9c00a2a970d6d3e857ec
Khirulnizam Abd Rahman <khirulnizam@gmail.com>
2020-01-16 14:41:29 +0800

    Succesfully push record to firestore

--- makanapps/lib/insertpage.dart
+++ makanapps/lib/insertpage.dart
@@ -1,55 +1,61 @@
 //class InsertPage
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:toast/toast.dart';
 
-class InsertPage extends StatelessWidget{
+class InsertPage extends StatelessWidget {
+
   final db = Firestore.instance;
-  TextEditingController _name, _votes, _price = TextEditingController();
-  @override
+  TextEditingController _name = TextEditingController();
+  TextEditingController _votes = TextEditingController();
+  TextEditingController _price = TextEditingController();
+  //TextEditingController _name, _votes, _price = TextEditingController();
+  //@override
   Widget build(BuildContext context){
     //our code
     return Scaffold(
-      appBar: new AppBar(title: new Text('Insert Page'),),
+      appBar: new AppBar(title: new Text('Insert Menu '
+          'Page'),),
       body: ListView(
         padding: EdgeInsets.all(12.0),
         children: <Widget>[
           Padding(//TextField ID
-            padding: EdgeInsets.symmetric(vertical: 15.0),
+            padding: EdgeInsets.symmetric(vertical: 10.0,horizontal: 15.0),
             child: TextField(
               controller: _name,
               decoration: InputDecoration(hintText: 'enter food menu name'),
             ),
           ),
           Padding(//TextField name
-            padding: EdgeInsets.symmetric(vertical: 15.0),
+            padding: EdgeInsets.symmetric(vertical: 10.0,horizontal: 15.0),
             child: TextField(
               controller: _price,
-              decoration: InputDecoration(hintText: 'enter menu price'),
+              decoration: InputDecoration(hintText: 'menu price (RM)'),
             ),
           ),
           Padding(//TextField name
-            padding: EdgeInsets.symmetric(vertical: 15.0),
+            padding: EdgeInsets.symmetric(vertical: 10.0,horizontal: 15.0),
             child: TextField(
               controller: _votes,
               decoration: InputDecoration(hintText: 'initial vote'),
             ),
           ),
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(10.0),
             child: RaisedButton(
               child: Text('Add'),
               color: Colors.green,
               onPressed: () async {
-                //auto-id provided by firebase
+                //auto-id provided by firestore
                 await db.collection('makanplace')
                 .add({'name': _name.text,
-                  'price': _price.text,
-                  'votes': _votes.text}
-                );
+                  'price': int.parse(_price.text.toString()),
+                  'votes': int.parse(_votes.text.toString())});
+                Toast.show("Firebase add", context, duration: Toast.LENGTH_LONG, gravity:  Toast.CENTER);
               },
             ),
           ),
-          SizedBox(height: 20.0),
+          SizedBox(height: 15.0),
           StreamBuilder<QuerySnapshot>(
               stream: db.collection('makanplace').snapshots(),
               builder: (context, snapshot) {
@@ -68,4 +74,4 @@ class InsertPage extends StatelessWidget{
     );
   }//end Widget
 
-}//end PageTwo
+}//end InsertPage
